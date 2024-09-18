faf42ca38822b542ddaf12c47d0e279eef9f5425
meghanadh317 <akkichettymeghanadh@gmail.com>
2024-09-18 16:05:34 +0530

    Dhonispercentage.py

--- Dhonispercentage.py
+++ Dhonispercentage.py
@@ -22,3 +22,12 @@ Sample Input and Output2:
 Note: Bold highlighted is the output
 
 '''
+
+s=int(input ())
+b=int(input ())
+e=int(input ())
+ti=s+b+e
+ps=(s/ti)*100
+pb=(b/ti)*100
+pe=(e/ti)*100
+print("%.2f"%ps,"%.2f"%pb,"%.2f"%pe)
